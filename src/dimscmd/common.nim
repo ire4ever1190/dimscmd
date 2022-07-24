724d8cc635b99a6f36fb8ba041036f015888e7d5
Asmoniel <danishi@disroot.org>
2022-07-24 19:01:15 -0400

    Remove unused fields in ProcParameter object

--- src/dimscmd/common.nim
+++ src/dimscmd/common.nim
@@ -28,8 +28,6 @@ type
         array*: bool
         options*: seq[EnumOption]     # only when an enum
         # Only when an array
-        minLength: int
-        maxLength: int
         help*: string
 
     CommandType* = enum
